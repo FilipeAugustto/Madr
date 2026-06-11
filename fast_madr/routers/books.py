0baf159b6edbf89b6a1f657d6aecf3430920a615
Filipe <filipeaugustto22@gmail.com>
2026-06-11 18:52:50 -0300

    refactor: troca da autorização de acesso da rota PATCH /books
    - Troca do acesso de admin para usuário comum

--- fast_madr/routers/books.py
+++ fast_madr/routers/books.py
@@ -69,7 +69,7 @@ def delete_book(book_id: int, admin_user: AdminUser, session: O_Session):
     '/{book_id}', status_code=HTTPStatus.OK, response_model=BookPublic
 )
 def patch_book(
-    book_id: int, book: BookUpdate, admin_user: AdminUser, session: O_Session
+    book_id: int, book: BookUpdate, current_user: CurrentUser, session: O_Session
 ):
     db_book = session.scalar(select(Book).where(Book.id == book_id))
 
